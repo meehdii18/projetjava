2dada497ab95c7b66caa2ccbb588fec7539deabe
Aissatou28 <cissaissatou25@gmail.com>
2024-05-14 16:04:07 +0200

    add files

---
+++ timeTrackerApp/src/main/java/javaProject/Model/company.java
@@ -0,0 +1,24 @@
+import javaProject.Model;
+
+public class Company {
+    private int companyId;
+    private char companyName;
+
+    public Company{
+        
+    }
+
+    public int getCompanyIdId(){
+        return companyId;
+    }
+    public void setCompanyId(int newId){
+        this.companyId = id;
+    }
+    public char getCompanyName(){
+        return companyName;
+    }
+    public void setCompanyName(int newName){
+        this.companyName = name;
+    }
+
+}
---
+++ timeTrackerApp/src/main/java/javaProject/Model/department.java
@@ -0,0 +1,27 @@
+import javaProject.Model;
+
+public class Department {
+    private int departmentId;
+    private char departmentName;
+    private int idCompany;
+
+    public Departement(){
+
+    }
+
+    public int getDepartmentId(){
+        return departmentId;
+    }
+
+    public void setDepartmentId(int newId){
+        this.departmentId = newId;
+    }
+
+    public char getDepartmentName(){
+        return departmentName;
+    }
+
+    public void setDepartmentName(char newName){
+        this.departmentName  = newName;
+    }
+}
