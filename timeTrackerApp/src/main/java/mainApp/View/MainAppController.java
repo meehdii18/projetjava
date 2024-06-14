9ad60a4df36593eea2b149f86039f292963c57ac
meehdii18 <95209113+meehdii18@users.noreply.github.com>
2024-06-14 10:26:22 +0200

    Edition et recherche d'employé fini

--- timeTrackerApp/src/main/java/mainApp/View/MainAppController.java
+++ timeTrackerApp/src/main/java/mainApp/View/MainAppController.java
@@ -2,12 +2,15 @@
 
 package mainApp.View;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import mainApp.Controller.ManageCompany;
 import mainApp.Model.*;
-
+import javafx.collections.ObservableList;
 import java.time.LocalTime;
 import java.util.Hashtable;
 import java.util.UUID;
@@ -72,6 +75,7 @@ public class MainAppController {
     @FXML
     private TableColumn<?, ?> timeColumnTT;
 
+
     // Input pour l'ajout des employés
 
     @FXML
@@ -95,7 +99,34 @@ public class MainAppController {
 
     // Onglet infos détaillées
     @FXML
+    private TableView<Employee> employeeDetailsTable;
+
+    @FXML
     private Tab employeeDetailsTab;
+
+    @FXML
+    private TableColumn<Employee, String> idColumn;
+
+    @FXML
+    private TableColumn<Employee, String> firstNameColumn;
+
+    @FXML
+    private TableColumn<Employee, String> lastNameColumn;
+
+    @FXML
+    private TableColumn<Employee, String> departmentColumn;
+
+    @FXML
+    private TableColumn<Employee, Integer> salaryColumn;
+
+    @FXML
+    private TableColumn<Employee, LocalTime> startHourColumn;
+
+    @FXML
+    private TableColumn<Employee, LocalTime> endHourColumn;
+
+    @FXML
+    private Button saveChangesButton;
 
     private Employee selectedEmployee;
 
@@ -122,14 +153,14 @@ public class MainAppController {
         ManageCompany controller = new ManageCompany(Company.deserializeCompany("timeTrackerApp/src/main/resources/data/company/Polytech.ser"),this);
         setController(controller);
 
-        // TODO : Régler le fait que les données ne sont pas bien sauvegardées / restaurés
-
         for (String departmentName : controller.getDepartments().keySet()) {
             inputDepartment.getItems().add(departmentName);
         }
-        System.out.println(controller.getCompanyName()+" company loading..."); // TODO : afficher le nom dans l'appli plutôt
+
+        // DEBUG
+        System.out.println(controller.getCompanyName()+" company loading...");
         System.out.println("Company object deserialized");
-        Hashtable<String,Department> departementlist =  controller.getDepartments(); // TODO : enlever si juste debug
+        Hashtable<String,Department> departementlist =  controller.getDepartments();
         System.out.println(departementlist);
 
         // Ajout de tous les employés dans la table
@@ -154,6 +185,36 @@ public class MainAppController {
         });
 
 
+        inputIn.textProperty().addListener((obs, oldText, newText) -> {
+            if (!newText.matches("\\d{2}:\\d{2}")) {
+                // Si le nouveau texte ne correspond pas au format "HH:MM", désactivez le bouton addEmployeeButton
+                addEmployeeButton.setDisable(true);
+            } else {
+                // Si le nouveau texte correspond au format "HH:MM", activez le bouton addEmployeeButton
+                addEmployeeButton.setDisable(false);
+            }
+        });
+
+        inputOut.textProperty().addListener((obs, oldText, newText) -> {
+            if (!newText.matches("\\d{2}:\\d{2}")) {
+                // Si le nouveau texte ne correspond pas au format "HH:MM", désactivez le bouton addEmployeeButton
+                addEmployeeButton.setDisable(true);
+            } else {
+                // Si le nouveau texte correspond au format "HH:MM", activez le bouton addEmployeeButton
+                addEmployeeButton.setDisable(false);
+            }
+        });
+
+        inputSalary.textProperty().addListener((obs, oldText, newText) -> {
+            if (!newText.matches("\\d+")) {
+                // Si le nouveau texte n'est pas un nombre, désactivez le bouton addEmployeeButton
+                addEmployeeButton.setDisable(true);
+            } else {
+                // Si le nouveau texte est un nombre, activez le bouton addEmployeeButton
+                addEmployeeButton.setDisable(false);
+            }
+        });
+
         tabPane.getSelectionModel().selectedItemProperty().addListener((obs, oldTab, newTab) -> {
             if (newTab == employeeDetailsTab && selectedEmployee == null) {
                 // Si l'utilisateur tente de sélectionner l'onglet des détails de l'employé sans avoir sélectionné un employé,
@@ -162,25 +223,78 @@ public class MainAppController {
             }
         });
 
+        // Rendre le tableau modifiable
+        employeeDetailsTable.setEditable(true);
+
+// Rendre les colonnes modifiables
+        firstNameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        firstNameColumn.setOnEditCommit(
+                (TableColumn.CellEditEvent<Employee, String> t) -> t.getTableView().getItems().get(
+                        t.getTablePosition().getRow()).setFirstName(t.getNewValue())
+        );
+
+        lastNameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        lastNameColumn.setOnEditCommit(
+                (TableColumn.CellEditEvent<Employee, String> t) -> t.getTableView().getItems().get(
+                        t.getTablePosition().getRow()).setLastName(t.getNewValue())
+        );
+
+        ObservableList<String> departmentNames = FXCollections.observableArrayList(controller.getDepartments().keySet());
+
+        // Configurer la colonne du département pour utiliser une ComboBox
+        departmentColumn.setCellFactory(ComboBoxTableCell.forTableColumn(departmentNames));
+        departmentColumn.setOnEditCommit(
+                (TableColumn.CellEditEvent<Employee, String> t) -> {
+                    Employee employee = t.getTableView().getItems().get(t.getTablePosition().getRow());
+                    employee.setDepartmentName(t.getNewValue());
+                }
+        );
+
+
+        // Recherche d'employé
+        ObservableList<Employee> allEmployees = FXCollections.observableArrayList();
+        for (Department department : controller.getDepartments().values()) {
+            allEmployees.addAll(department.getEmployeesList().values());
+        }
+
+        // Ajoutez un ChangeListener au champ searchEmployeeField
+        searchEmployeeField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue == null || newValue.isEmpty()) {
+                // Si le champ de recherche est vide, réinitialisez la table avec tous les employés
+                employeesTable.setItems(allEmployees);
+            } else {
+                // Sinon, filtrez la liste des employés en fonction du texte entré
+                ObservableList<Employee> filteredEmployees = FXCollections.observableArrayList();
+                for (Employee employee : allEmployees) {
+                    if (employee.getFirstName().toLowerCase().contains(newValue.toLowerCase())
+                            || employee.getLastName().toLowerCase().contains(newValue.toLowerCase())) {
+                        filteredEmployees.add(employee);
+                    }
+                }
+                employeesTable.setItems(filteredEmployees);
+            }
+        });
+
+
         tabPane.getTabs().remove(employeeDetailsTab);
 
         tabPane.getSelectionModel().select(0); // Select the first tab
         addEmployeeButton.setOnAction(event -> addEmployee());
         removeEmployeeButton.setOnAction(event -> deleteEmployee());
+        saveChangesButton.setOnAction(event -> saveChanges());
+
     }
 
     @FXML
     protected void addEmployee() {
-        // TODO : Code pour ajouter un employé, il faut l'enregistrer dans les données sérialisées et rafraichir la vue
-
         String id = UUID.randomUUID().toString(); // Générer un ID unique pour chaque employé
         String firstName = inputFirstName.getText();
         String lastName = inputLastName.getText();
         String departmentName = (String) inputDepartment.getValue();
         LocalTime start_hour = LocalTime.parse(inputIn.getText());
         LocalTime end_hour = LocalTime.parse(inputOut.getText());
-        LocalTime extra_hour = LocalTime.of(0, 0); // Vous pouvez modifier cela en fonction de vos besoins
-        String salaryString = inputSalary.getText(); // Vous pouvez modifier cela en fonction de vos besoins
+        LocalTime extra_hour = LocalTime.of(0, 0);
+        String salaryString = inputSalary.getText();
         int salary = Integer.parseInt(salaryString);
         ClockingHistory clockingHistory = new ClockingHistory();
 
@@ -215,8 +329,19 @@ public class MainAppController {
         // Mettez à jour l'employé sélectionné
         this.selectedEmployee = employee;
 
-        // Activez l'onglet
-        employeeDetailsTab.setDisable(false);
+        // Remplissez le TableView avec les informations de l'employé
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        firstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        lastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        departmentColumn.setCellValueFactory(new PropertyValueFactory<>("departmentName"));
+        salaryColumn.setCellValueFactory(new PropertyValueFactory<>("salary"));
+        startHourColumn.setCellValueFactory(new PropertyValueFactory<>("startHour"));
+        endHourColumn.setCellValueFactory(new PropertyValueFactory<>("endHour"));
+
+        ObservableList<Employee> employeeDetails = FXCollections.observableArrayList();
+        employeeDetails.add(employee);
+        employeeDetailsTable.setItems(employeeDetails);
+
 
         // Ajoutez l'onglet au TabPane
         tabPane.getTabs().add(employeeDetailsTab);
@@ -224,8 +349,11 @@ public class MainAppController {
         // Sélectionnez l'onglet
         tabPane.getSelectionModel().select(employeeDetailsTab);
 
+    }
 
-        // Désactivez l'onglet
-        employeeDetailsTab.setDisable(true);
+    private void saveChanges() {
+        controller.saveData();
+        employeesTable.refresh();
+        employeeDetailsTable.refresh();
     }
 }
